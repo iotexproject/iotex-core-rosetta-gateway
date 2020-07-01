7515c929e6e3f5b22355460d6c592beee3943758
lzxm160 <lzxm160@sina.com>
2020-07-01 09:38:20 +0800

    fix fee 0

--- tests/test.sh
+++ tests/test.sh
@@ -24,7 +24,7 @@ cd tests
 ./rosetta-cli create:configuration config.json
 
 printf "${GRN}### Validating Rosetta gateway implementation...${OFF}\n"
-./rosetta-cli check --lookup-balance-by-block false --end 10 --bootstrap-balances ./bootstrap_balances.json --block-concurrency 8
+./rosetta-cli check --lookup-balance-by-block=false --end 100 --bootstrap-balances ./bootstrap_balances.json --block-concurrency 8
 ./rosetta-cli view:account '{"address":"io10t7juxazfteqzjsd6qjk7tkgmngj2tm7n4fvrd"}'
 ./rosetta-cli view:block 4034780
 rm -rf /tmp/rosetta-cli*
