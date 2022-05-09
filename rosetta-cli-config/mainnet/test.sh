8899fdbe47eb2ac43ea5910473b41b6e4ee0400c
millken <millken@gmail.com>
2022-05-09 16:31:13 +0800

    update mainnet config

--- rosetta-cli-config/mainnet/test.sh
+++ rosetta-cli-config/mainnet/test.sh
@@ -3,7 +3,7 @@ RED=$'\e[31;1m'
 OFF=$'\e[0m'
 
 printf "${GRN}### Run rosetta-cli check:data${OFF}\n"
-rosetta-cli-0.7.2 check:data --configuration-file ./iotex.json >/dev/null 2>&1 &
+rosetta-cli-0.7.2 check:data --configuration-file ./mainnet.config.json >/dev/null 2>&1 &
 dataCheckPID=$!
 wait $dataCheckPID
 if [ "$?" == 0 ]
